817a48ae20604ea8591ad020c89e3cb8cf95879a
The Metaist <metaist@metaist.com>
2024-07-04 23:16:33 -0400

    add: opengraph tags (closes #11)

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,6 +2,7 @@ module.exports = {
   title: "Metaist",
   url: "https://metaist.com/",
   language: "en",
+  color: "#336699",
   description: "Projects and notes from Metaist LLC",
   author: {
     name: "The Metaist",
